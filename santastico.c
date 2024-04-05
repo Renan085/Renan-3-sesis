840f438315acfe34791003b5a11948ed390cfe58
Renan085 <127419427+Renan085@users.noreply.github.com>
2024-04-05 08:47:40 -0300

    Create santastico.c

---
+++ santastico.c
@@ -0,0 +1,18 @@
+int main() {
+char c = 'a';
+int x = 5;
+float f = 5.25;
+double d = 15.673;
+double y = 5.0e10;
+
+int numeros[5] = {1,2,3,4,5};
+for (size t i = 0; i < 4; 1++);
+{
+    print("%d \n" , numeros[i]);
+}
+printf("o valor é c =%c \n",c);
+printf("o valor é x =%x \n",x);
+printf("o valor é f =%f \n",f);
+printf("o valor é d =%d \n",d);
+printf("o valor é e =%e \n",e);
+}
